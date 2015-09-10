48a710d8030e505a4dea5a6d421c79e6e749e201
Kenneth Lewenhagen <lewenhagen@live.com>
2015-09-10 15:34:57 +0200

    struct fix

--- picGame/js/main.js
+++ picGame/js/main.js
@@ -1,23 +1,48 @@
 (function() {
     'use strict';
 
-    var placeImage, txt, btn, result, maxRounds, currRound, corrRand, errRand, completed, score, resultText, corrText;
-    var audio = new Audio();
+    var placeImage, txt, btn, result, maxRounds, currRound, corrRand, errRand, completed, score, resultText, corrText, progBar, soundBtn, sound;
+    //var audio = new Audio();
 
 
     maxRounds = questions.length;
     score = 0;
     completed = [];
+    sound = true;
     currRound = Math.floor(Math.random() * questions.length);
     corrRand = Math.floor(Math.random() * corrImages.length);
     errRand = Math.floor(Math.random() * errImages.length);
     //corrText = Math.floor(Math.random() * corrTexts.length);
     corrText = "JÄTTEBRA!";
 
+    soundBtn = document.getElementById("sound");
     placeImage = document.getElementById("img");
     result = document.getElementById("right-top");
     resultText = document.getElementById("right-bottom");
     btn = document.getElementById("checkBtn");
+    progBar = document.getElementById("progBar");
+
+    function toggleSound() {
+        if (sound) {
+            soundBtn.innerHTML = "Ljudet är av";
+            soundBtn.classList.add("btn-danger");
+            soundBtn.classList.remove("btn-success");
+
+            sound = false;
+        } else {
+            soundBtn.innerHTML = "Ljudet är på";
+            soundBtn.classList.add("btn-success");
+            soundBtn.classList.remove("btn-danger");
+            sound = true;
+        }
+    }
+
+    function setProgressBar () {
+        var total = questions.length;
+        var one = (100 / total);
+        var old = parseInt(progBar.style.width);
+        progBar.style.width = (one+old) + "%";
+    }
 
     function nextQuestion() {
         corrRand = Math.floor(Math.random() * corrImages.length);
@@ -46,6 +71,7 @@
                 result.innerHTML = "";
                 resultText.innerHTML = "";
                 input.value = "";
+                setProgressBar();
                 nextQuestion();
             }, 3000);
             result.innerHTML = corrImages[corrRand];
@@ -68,9 +94,17 @@
         var str = document.getElementById("focusedInput").value.split("");
         var newStr = str.join(", ");
 
-        audio.src ="http://translate.google.com/translate_tts?ie=utf-8&tl=sv&q=" + newStr;
-        audio.play();
+        var u1 = new SpeechSynthesisUtterance(newStr);
+                u1.lang = 'sv-SE';
+                u1.pitch = 1;
+                u1.rate = 1;
+                //u1.voice = voices[10];
+                u1.voiceURI = 'native';
+                u1.volume = 1;
+                speechSynthesis.speak(u1);
 
+        // audio.src ="../config/sound.php?tl=sv&q=" + newStr;
+        // audio.type = "audio/mpeg";
     }
 
     function init() {
@@ -79,11 +113,23 @@
 
 
     btn.addEventListener("click", function(event) {
-        playLetters();
+        if (sound) {
+            playLetters();
+        } else {
+            checkAnswer();
+        }
     });
 
-    audio.addEventListener("ended", function() {
-        checkAnswer();
+    // audio.addEventListener("ended", function() {
+    //     checkAnswer();
+    // });
+    //
+    // audio.addEventListener("load", function() {
+    //     audio.play();
+    // }, true);
+
+    soundBtn.addEventListener("click", function() {
+        toggleSound();
     });
 
     init();
