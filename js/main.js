7d45bd0f83d94e3d6c4bff831784d391e06f26ad
Kenneth Lewenhagen <lewenhagen@live.com>
2015-09-10 10:09:36 +0200

    almost finished

--- js/main.js
+++ js/main.js
@@ -1,33 +1,56 @@
 (function() {
     'use strict';
 
-    var placeImage, txt, btn, result, maxRounds, currRound, corrRand, errRand;
+    var placeImage, txt, btn, result, maxRounds, currRound, corrRand, errRand, completed, score;
 
     maxRounds = questions.length;
-    currRound = 0;
+    score = 0;
+    completed = [];
+    currRound = Math.floor(Math.random() * questions.length);
     corrRand = Math.floor(Math.random() * corrImages.length);
+    errRand = Math.floor(Math.random() * errImages.length);
 
     placeImage = document.getElementById("img");
     result = document.getElementById("right");
     btn = document.getElementById("checkBtn");
 
+    function nextQuestion() {
+        if (completed.indexOf(currRound) === -1) {
+            document.getElementById("focusedInput").focus();
+            placeImage.src = questions[currRound].img;
+        } else if (completed.length === questions.length) {
+            document.getElementById("left").innerHTML = "POÄNG: " + score;
+            result.innerHTML = "ALLA KLARA!";
+        } else {
+            currRound = Math.floor(Math.random() * questions.length);
+            nextQuestion();
+        }
+    }
+
     function checkAnswer() {
-        var checkThis = document.getElementById("focusedInput").value;
+        var input = document.getElementById("focusedInput");
+        var checkThis = input.value;
 
         if (checkThis.toUpperCase() === questions[currRound].corr) {
             window.setTimeout(function() {
-                result.innerHTML = "bte";
-                // nextQuestion();
+                completed.push(currRound);
+                score++;
+                result.innerHTML = "";
+                input.value = "";
+                nextQuestion();
             }, 4000);
             result.innerHTML = corrImages[corrRand];
         } else {
-            result.innerHTML = "NOOOOOOOO";
+            window.setTimeout(function() {
+                result.innerHTML = "";
+                input.value = "";
+            }, 3000);
+            result.innerHTML = errImages[errRand];
         }
     }
 
     function init() {
-        var currImg = questions[currRound].img;
-        placeImage.src = currImg;
+        placeImage.src = questions[currRound].img;
     }
 
 
--- js/words.js
+++ js/words.js
@@ -7,14 +7,91 @@ var questions = [
         img: "img/hund.png",
         corr: "HUND",
         done: false
+    }, {
+        img: "img/ko.jpg",
+        corr: "KO",
+        done: false
+    }, {
+        img: "img/boat.png",
+        corr: "BÅT",
+        done: false
+    }, {
+        img: "img/boy.jpg",
+        corr: "POJKE",
+        done: false
+    }, {
+        img: "img/girl.jpg",
+        corr: "FLICKA",
+        done: false
+    }, {
+        img: "img/majs.jpg",
+        corr: "MAJS",
+        done: false
+    }, {
+        img: "img/bil.jpg",
+        corr: "BIL",
+        done: false
+    }, {
+        img: "img/bus.jpg",
+        corr: "BUSS",
+        done: false
+    }, {
+        img: "img/stol.jpg",
+        corr: "STOL",
+        done: false
+    }, {
+        img: "img/hus.jpg",
+        corr: "HUS",
+        done: false
+    }, {
+        img: "img/mus.png",
+        corr: "MUS",
+        done: false
+    }, {
+        img: "img/orm.jpg",
+        corr: "ORM",
+        done: false
+    }, {
+        img: "img/pig.jpg",
+        corr: "GRIS",
+        done: false
+    }, {
+        img: "img/horse.jpg",
+        corr: "HÄST",
+        done: false
+    }, {
+        img: "img/sheep.jpg",
+        corr: "FÅR",
+        done: false
+    }, {
+        img: "img/elsa.jpg",
+        corr: "ELSA",
+        done: false
+    }, {
+        img: "img/anna.jpg",
+        corr: "ANNA",
+        done: false
+    }, {
+        img: "img/snowwhite.jpg",
+        corr: "SNÖVIT",
+        done: false
+    }, {
+        img: "img/olof.png",
+        corr: "OLOF",
+        done: false
+    }, {
+        img: "img/sven.jpg",
+        corr: "SVEN",
+        done: false
     }
 ];
 
 var corrImages = [
-    "<img src='img/corr.jpg'>",
-    "<img src='img/corr1.jpg'>"
+    "<img src='img/correct/corr.jpg'>",
+    "<img src='img/correct/corr2.jpg'>"
 ];
 
-var wrongImages = [
-
+var errImages = [
+    "<img src='img/error/err1.jpg'>",
+    "<img src='img/error/err2.jpg'>",
 ];
